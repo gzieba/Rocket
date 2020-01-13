fbb21993daed4525a4e09eaf420bde632f2c7ff5
Gniewomir Zieba <gniewomir.zieba@gmail.com>
2020-01-13 15:12:54 +0100

    Fixing error with rotating scene.

--- Assets/Scripts/SkyboxRotate.cs
+++ Assets/Scripts/SkyboxRotate.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class SkyboxRotate : MonoBehaviour
 {
     [SerializeField] Vector3 increment = new Vector3(-1.0f, 1.0f, 1.0f);
+
+    Skybox skybox;
+
     // Start is called before the first frame update
     void Start()
     {
+        skybox = GetComponent<Skybox>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(increment * Time.deltaTime);
+        skybox.transform.Rotate(increment);
     }
 }
